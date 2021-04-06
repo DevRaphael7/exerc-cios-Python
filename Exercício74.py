f82e9e75b7a34434073c8e60932c5a0085c12418
DevRaphael7 <raphaelramalho1989@gmail.com>
2021-04-06 14:09:06 -0300

    Inserindo texto na entrada

--- Exercício74.py
+++ Exercício74.py
@@ -1,4 +1,4 @@
-nota1, nota2, nota3, nota4 = float(input()), float(input()), float(input()), float(input())
+nota1, nota2, nota3, nota4 = float(input('Digite a nota 1: ')), float(input('Digite a nota 2: ')), float(input('Digite a nota 3: ')), float(input('Digite a nota 4: '))
 
 mean = (nota1 + nota2 + nota3 + nota4)/ 4
 
